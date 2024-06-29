1c7e74082225015ce2490bf50ddc724e82a6bf4c
neeru8jangid <neeru.22ejics103@jietjodhpur.ac.in>
2024-06-29 10:41:25 +0530

    seconnd commit

---
+++ day2.py
@@ -0,0 +1,10 @@
+#list 
+lst2 =[1,12,14,[12,33,44]]
+#to fetch 33 from list
+print(lst2[3][1])
+lst= [10,20,30,40,50,60,70]
+#insertion
+#lst[2] = 200 #it will overright on 30
+#lst.append(200) #500 at the end of the list
+
+
